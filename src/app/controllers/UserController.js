b97397856d849131eb3ae1bd5338e8497579e5a7
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-07 22:39:48 -0300

    add index method in user's controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,11 @@
 import User from '../models/User';
 
 class UserController {
+  async index(req, res) {
+    const users = await User.findAll();
+    return res.json(users);
+  }
+
   async store(req, res) {
     const userExists = await User.findOne({ where: { name: req.body.name } });
     if (userExists)
