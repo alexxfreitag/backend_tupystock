29c705953a3df48bce4223b2fba233871f0c7ff4
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-07 22:40:16 -0300

    include get and post requests in routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import OrderController from './app/controllers/OrderController';
 
 const routes = new Router();
 
@@ -8,6 +9,8 @@ routes.get('/', (req, res) => {
   return res.json({ message: 'routes test' });
 });
 
+routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+routes.post('/orders', OrderController.store);
 
 export default routes;
