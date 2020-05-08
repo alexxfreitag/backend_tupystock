a88049c87fe45401deaa43bf6337c7debf0dcdb2
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-07 22:27:15 -0300

    verify if user already exists when creating a new one

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,21 +2,15 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const userExists = await User.findOne({ where: { name: req.body.name } });
+    if (userExists)
+      return res
+        .status(400)
+        .json({ error: 'Já existe um usuário com esse nome.' });
+
     const { id, name } = await User.create(req.body);
 
-    return res.json({
-      id,
-      name,
-    });
-  }
-
-  async update(req, res) {
-    /* const { id, name } = await user.update(req.body);
-
-    return res.json({
-      id,
-      name,
-    }); */
+    return res.json({ id, name });
   }
 }
 
