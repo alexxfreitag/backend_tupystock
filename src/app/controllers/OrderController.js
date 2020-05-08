eba99fe36702440f107b0098de334926c8f407d1
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-07 22:43:14 -0300

    add index method in order's controller

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,6 +1,11 @@
 import Order from '../models/Order';
 
 class OrderController {
+  async index(req, res) {
+    const orders = await Order.findAll();
+    return res.json(orders);
+  }
+
   async store(req, res) {
     const { id, user_id, total_amount } = await Order.create(req.body);
 
