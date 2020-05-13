ad5743b354d6718f21f2daa703be420e4ae5930d
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-13 13:54:39 -0300

    update database config for production

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100',
+  host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'tupystockdeploy',
   database: 'tupystock',
   define: {
     timestamps: true,
