39d49e2f9426a617c390ecc0a09b445b22885076
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-13 22:06:40 -0300

    add enviroments variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'tupystockdeploy',
-  database: 'tupystock',
+  host: process.env.NODE_ENV === 'development' ? 'localhost' : '192.168.99.100',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
