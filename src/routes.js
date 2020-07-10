119287b5348b7a7e462669055dea953849646d08
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-07-10 17:01:25 -0300

    add route for files uploads

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import OrderController from './app/controllers/OrderController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
 import ProductsOrderController from './app/controllers/ProductsOrderController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'routes test' });
@@ -20,5 +24,6 @@ routes.post('/sessions', SessionController.store);
 routes.get('/products', ProductController.index);
 routes.post('/products', ProductController.store);
 routes.get('/products_order', ProductsOrderController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
